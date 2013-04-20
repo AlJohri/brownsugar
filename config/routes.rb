63d12d97d9deebbb029bda56c36c8c6546a73860
Al Johri <al.johri@gmail.com>
2013-04-20 10:05:34 -0500

    BrownSugar Initial commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Brownsugar::Application.routes.draw do
+  
+  ComfortableMexicanSofa::Routing.admin(:path => '/cms-admin')
+  
+  # Make sure this routeset is defined last
+  ComfortableMexicanSofa::Routing.content(:path => '/', :sitemap => false)
+
   root :to => "home#index"
 end
