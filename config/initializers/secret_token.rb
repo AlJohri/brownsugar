1efd8057c3e6ac390dbaf846988a7e3906e79969
Al Johri <al.johri@gmail.com>
2013-04-20 05:03:48 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brownsugar::Application.config.secret_token = '0a735d7b846054e7fc7abf7b5afd67996108ac75ce8b0d3da97ed8037825fe67fb6431d8c44d6fc6fd274bbddb74d2d8c2956f0d84cba6759514ab2e7b945cb6'
